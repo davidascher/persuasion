2bfe34c428c3db6d13fce118458250e92669396c
David Ascher <david.ascher@gmail.com>
2010-10-10 23:03:49 -0700

    more real tests, but still hacky

--- test/basic.test.js
+++ test/basic.test.js
@@ -7,7 +7,16 @@ var app = require('../app');
 
 
 module.exports = {
-    'test basic server': function(assert){
+    setup: function(test) {
+        console.log('setup called');
+        app.redis.select(15, function(err, ok) {
+            app.redis.flushdb(function(err, ok) {
+                console.log('selected test DB and flushed it');
+                test();
+            });
+        });
+    },
+    basic: function(assert){
         assert.response(app.app,
             { url: '/' },
             function(res){
@@ -17,10 +26,28 @@ module.exports = {
             { url: '/newslide' },
             { status: 200 },
             function(res){
-                console.log("BODY", res.body);
                 assert.ok(res.body.indexOf('does not exist') >= 0, 'Test assert.response() callback');
-                app.redis.end(); // last test.
             });
-
+        assert.response(app.app,
+            { url: '/create/newslide' },
+            { status: 303 },
+            function(res){
+                assert.equal('/newslide', res.headers.location, 'Test assert.response() callback');
+                assert.ok(res.body.indexOf('redirecting to ') >= 0, 'Test assert.response() callback');
+            });
+        assert.response(app.app,
+            { url: '/newslide' },
+            { status: 200 },
+            function(res){
+                // the slide exists now!
+                assert.ok(res.body.indexOf('<header>') >= 0, 'Test assert.response() callback');
+            });
+        // finish by closing of the redis connection
+        // there should be a better way to do this XXX!!
+        assert.response(app.app,
+            { url: '/' },
+            function(res){
+                app.redis.end();
+            });
     },
 };
